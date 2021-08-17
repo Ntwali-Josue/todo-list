d634e7eb7d4df0e6254bcb39298f183085eaaaaa
Ntwali-Josue <kntwalijos@gmail.com>
2021-08-18 00:42:27 +0200

    Create array of tasks and populate each in a list element

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-const todoList = document.querySelector('.list');
+const todoList = document.querySelector('.item');
+const todoDescription = document.querySelector('.todo-description');
+const addList = document.querySelector('.add');
 
 const todo = [
-  "description" = '',
-  "completed" = Boolean,
-  "index" = Number
+  {
+  "description" : 'Todo list one',
+  "completed" : true,
+  "index" : 1
+  },
+  {
+    "description" : 'Todo list two',
+    "completed" : false,
+    "index" : 2
+  },
+  {
+    "description" : 'Todo list three',
+    "completed" : false,
+    "index" : 3
+  }
 ];
 
+const displayList = () => {
+  const list = todo.map((item) => `<div>${item.description}</div>`)
+  console.log(list);
+}
 
+addList.addEventListener('click', displayList);
