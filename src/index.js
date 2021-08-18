c76908383a66f70f907394cc77d22c29d3ed303e
Ntwali-Josue <kntwalijos@gmail.com>
2021-08-18 17:50:47 +0200

    Add display list function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const todoList = document.querySelector('.item');
-const todoDescription = document.querySelector('.todo-description');
+const inputTodo = document.querySelector('.todo-input');
 const addList = document.querySelector('.add');
 
 const todo = [
   {
   "description" : 'Todo list one',
-  "completed" : true,
+  "completed" : false,
   "index" : 1
   },
   {
@@ -21,8 +21,14 @@ const todo = [
 ];
 
 const displayList = () => {
-  const list = todo.map((item) => `<div>${item.description}</div>`)
-  console.log(list);
+  let populateList;
+  for(let i = 0; i<todo.length; i++) {
+    const list = document.createElement('li');
+    list.innerHTML += `${todo[i].description}`;
+    console.log(list);
+    populateList = todoList.appendChild(list);
+  }
+  return populateList;
 }
 
-addList.addEventListener('click', displayList);
+displayList();
